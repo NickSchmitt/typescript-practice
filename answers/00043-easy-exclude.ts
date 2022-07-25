c8e8e6daa504301bfde41775592e126309402475
nickschmitt <nickschmitt@gmail.com>
2022-07-24 17:29:39 -0700

    add documentation to explain MyExclude behavior

--- answers/00043-easy-exclude.ts
+++ answers/00043-easy-exclude.ts
@@ -1,2 +1,3 @@
 // string union (T) extends the narrower string literal (U)
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
 type MyExclude<T, U> = T extends U ? never : T
